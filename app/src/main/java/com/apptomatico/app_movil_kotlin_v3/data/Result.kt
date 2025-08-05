7bc4d8c90db226c9d57c4b1d8659e5bc070f7a85
jjhdevelop <jescamillla2016@hotmail.com>
2025-08-05 15:40:14 -0600

    Carga inicial del proyecto Android

---
+++ app/src/main/java/com/apptomatico/app_movil_kotlin_v3/data/Result.kt
@@ -0,0 +1,18 @@
+package com.apptomatico.app_movil_kotlin_v3.data
+
+/**
+ * A generic class that holds a value with its loading status.
+ * @param <T>
+ */
+sealed class Result<out T : Any> {
+
+    data class Success<out T : Any>(val data: T) : Result<T>()
+    data class Error(val exception: Exception) : Result<Nothing>()
+
+    override fun toString(): String {
+        return when (this) {
+            is Success<*> -> "Success[data=$data]"
+            is Error -> "Error[exception=$exception]"
+        }
+    }
+}
---
+++ app/src/main/java/com/apptomatico/app_movil_kotlin_v3/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.apptomatico.app_movil_kotlin_v3.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
